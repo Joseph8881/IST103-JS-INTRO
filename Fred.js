17627d0c79a6b1deb5441bef0f5989a2e6b4d1e0
Joseph8881 <fredpoku88@gmail.com>
2025-08-15 20:40:49 -0700

    Update Fred.js
    assngmt 2

--- Fred.js
+++ Fred.js
@@ -1,2 +1,16 @@
 
 alert("hello, world");
+
+
+// hello world
+
+
+const person_name="Joseph"
+const person_age=50
+const he_is_a_teacher="false"
+const he_has_a_car=null
+
+console.log("person_name"+" is name")
+console.log("person_age"+" is age")
+console.log(" he_is_a_teacher"+"is profession")
+console.log(" he_has_a_car"+"is possession")
