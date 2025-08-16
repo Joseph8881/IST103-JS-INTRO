90ac5eccd94c4a43a4bde03d159022eb91b37eaf
Joseph8881 <fredpoku88@gmail.com>
2025-08-15 22:46:22 -0700

    Update Fred.js

--- Fred.js
+++ Fred.js
@@ -9,24 +9,23 @@ const person_age=50
 const he_is_a_teacher="false"
 const he_has_a_car=null
 
-console.log("person_name"+"is name");
+console.log("person_name"+"is name")
 console.log("person_age"+" is age")
-console.log("he_is_a_teacher"+"is profession");
-console.log("he_has_a_car"+"is possession");
+console.log("he_is_a_teacher"+"is profession")
+console.log("he_has_a_car"+"is possession")
 
 const to_do_list =["washing","cooking","studing","panting","swimming"]
 
 let speaker={brand:"JBL",make:"Flip 5",color:"red",size:5}
 
 
-console.log("washing"+" is clinsing clothes");
-console.log("cooking"+" is preparing food");
-console.log("studing"+" is learning book");
-console.log("swimming"+"is playing in the pool");
+console.log("washing"+" is clinsing clothes")
+console.log("cooking"+" is preparing food")
+console.log("studing"+" is learning book")
+console.log("swimming"+"is playing in the pool")
 
-
-console.log(brand,"JBL"+" is phone brand");
-console.log(make,"Flip 5"+"is phone make");
-console.log(color,"red"+"is phone color");
-console.log(size,5+"is phone size");
+console.log(brand,"JBL"+" is phone brand")
+console.log(make,"Flip 5"+"is phone make")
+console.log(color,"red"+"is phone color")
+console.log(size,5+"is phone size")
 
